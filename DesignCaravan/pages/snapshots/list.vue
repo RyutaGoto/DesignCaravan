a00a2ab70a4a4070f487359398ab035c9299fa37
RyutaGoto <kasa8gasa2rs6@gmail.com>
2019-10-19 00:59:39 +0100

    get the data from json file

--- DesignCaravan/pages/snapshots/list.vue
+++ DesignCaravan/pages/snapshots/list.vue
@@ -3,21 +3,8 @@
     div#body
       Subtitle(jp="旅のきろく", en="Snapshots")
       div.columns.is-multiline.is-mobile.is-centered
-        div.column(v-for="item in snapshots")
-          Snapshots.is-narrow(:title="item.title", :imgSrc="item.img", :obj="item")
-        //Snapshots.item.column.is-half(title="イカす写真", imgSrc="https://bulma.io/images/placeholders/1280x960.png")
-        //div.link
-          div
-            a.first &lt;&lt;
-          div
-            ul
-              li 1
-              li 2
-              li 3
-              li 4
-              li 5
-          div
-            a.last &gt;&gt;
+        div.column(v-for="item in json_data")
+          Snapshots.is-narrow(:title="item.name", :imgSrc="item.item", :obj="item")
     div#footer
 
 </template>
@@ -27,6 +14,8 @@ import Card from '~/components/Card'
 import Snapshots from '~/components/Snapshots'
 import Subtitle from '~/components/Subtitle'
 
+const axios = require('axios');
+
 export default {
   name: 'SnapshotsPage',
   components: {
@@ -35,50 +24,13 @@ export default {
     Subtitle
   },
   data: function(){
-    return{
-      snapshots:[
-        {
-          img: require('~/assets/snapshots/1.jpg'),
-          title: "サービスエリアで休憩",
-          date: "2019.July.1",
-        },
-        {
-          img: require('~/assets/snapshots/2.jpg'),
-          title: "札持ちとりっぴーさん",
-          date: "2019.June.28",
-        },
-        {
-          img: require('~/assets/snapshots/3.jpg'),
-          title: "すどやん！！",
-          date: "2019.June.28",
-        },
-        {
-          img: require('~/assets/snapshots/4.jpg'),
-          title: "ぐーな二人",
-          date: "2019.June.28",
-        },  
-        {
-          img: require('~/assets/snapshots/5.jpg'),
-          title: "キューブ",
-          date: "2019.June.25",
-        },
-        {
-          img: require('~/assets/snapshots/6.jpg'),
-          title: "いろんな松ぼっくり",
-          date: "2019.July.1",
-        },
-        {
-          img: require('~/assets/snapshots/7.jpg'),
-          title: "ダークマター",
-          date: "2019.June.28",
-        },
-        {
-          img: require('~/assets/snapshots/8.jpg'),
-          title: "膝が…",
-          date: "2019.July.1",
-        },      
-      ]
-    }
+  },
+  asyncData: async function({params}){
+    let url = "https://designcaravan-60b57.firebaseio.com/snapshots.json";
+    let result = await axios.get(url);
+    return {
+      json_data: result.data,
+    };
   }
 }
 </script>
